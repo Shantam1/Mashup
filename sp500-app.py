8a05a800430c3a3d8faf43be8d096d2e4c89cb79
Shantam1 <117478558+Shantam1@users.noreply.github.com>
2023-02-12 23:23:27 +0530

    Update sp500-app.py

--- sp500-app.py
+++ sp500-app.py
@@ -1,82 +1,107 @@
 import streamlit as st
-from pytube import YouTube
-from moviepy.editor import *
-import os
 import smtplib
+from email.mime.audio import MIMEAudio
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
-from email.utils import COMMASPACE
-from email import encoders
-import zipfile
+from pytube import YouTube
+from youtube_search import YoutubeSearch
+import pydub
+from pydub import AudioSegment
+import time
+import requests
+# from dotenv.main import load_dotenv
+# load_dotenv()
+st.set_page_config("Mashup🥁")
+st.title("Mashup 🥁")
 
-st.title('Mashup Web Service')
+def download_audio_from_search(singer, n, m):
+    results = YoutubeSearch(singer, max_results=m).to_dict()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    for i, result in enumerate(results):
+        video_url = "https://www.youtube.com/watch?v=" + result["id"]
+#         try:
+        yt = YouTube(requests.get(video_url, headers=headers).text)
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download(filename=f"{singer}_{i}.mp4")
 
-singer_name = st.text_input('Enter singer name')
-num_videos = st.number_input('Enter number of videos', min_value=1)
-video_duration = st.number_input('Enter duration of each video (in seconds)', min_value=1)
-email = st.text_input('Enter your email address')
+        audio = pydub.AudioSegment.from_file(f"{singer}_{i}.mp4")
+        audio = audio[:n * 1000]
+        audio.export(f"{singer}_{i}.mp3", format="mp3")
+        
+#         except:
+        time.sleep(3)
+#             continue
+# def download_audio_from_search(singer, n, m):
+#     results = YoutubeSearch(singer, max_results=m).to_dict()
+#     for i, result in enumerate(results):
+#         video_url = "https://www.youtube.com/watch?v=" + result["id"]
+#         yt = YouTube(video_url)
+#         stream = yt.streams.filter(only_audio=True).first()
+#         stream.download(filename=f"{singer}_{i}.mp4")
 
-if st.button('Create Mashup'):
-    # Download N videos of X singer from Youtube
-    search_query = YouTube("https://www.youtube.com/results?search_query=" + singer_name.replace(" ", "+"))
-    videos = search_query.filter(type="video")[:num_videos]
-    for video in videos:
-        YouTube(video).streams.filter(only_audio=True).first().download()
+#         audio = pydub.AudioSegment.from_file(f"{singer}_{i}.mp4")
+#         audio = audio[:n * 1000]
+#         audio.export(f"{singer}_{i}.mp3", format="mp3")
+        # st.write(f"Audio of {n} seconds from {yt.title} downloaded and converted to MP3 successfully")
 
-    # Convert all the videos to audio
-    for video_file in os.listdir():
-        if video_file.endswith(".mp4"):
-            video_path = os.path.abspath(video_file)
-            audio_file = os.path.splitext(video_file)[0] + ".mp3"
-            audio_path = os.path.abspath(audio_file)
-            VideoFileClip(video_path).audio.write_audiofile(audio_path)
-            os.remove(video_path)
+def combine_audio_files(singer, n, m):
+    combined = AudioSegment.empty()
+    for i in range(m):
+        audio = AudioSegment.from_file(f"{singer}_{i}.mp3")
+        combined += audio
+    combined.export(f"combined.mp3", format="mp3")
+    # st.write(f"All audio files combined into a single file: {singer}_combined.mp3")
 
-    # Cut first Y sec audios from all downloaded files
-    for audio_file in os.listdir():
-        if audio_file.endswith(".mp3"):
-            audio_path = os.path.abspath(audio_file)
-            cut_file = os.path.splitext(audio_file)[0] + "_cut.mp3"
-            cut_path = os.path.abspath(cut_file)
-            AudioFileClip(audio_path).subclip(0, video_duration).write_audiofile(cut_path)
-            os.remove(audio_path)
-
-    # Merge all audios to make a single output file
-    audio_files = [audio for audio in os.listdir() if audio.endswith("_cut.mp3")]
-    audio_clips = [AudioFileClip(audio) for audio in audio_files]
-    final_clip = concatenate_audioclips(audio_clips)
-    final_clip.write_audiofile("output.mp3")
-
-    # Zip the output file and send it via email
-    zip_filename = "output.zip"
-    with zipfile.ZipFile(zip_filename, 'w') as myzip:
-        myzip.write("output.mp3")
-    send_email(email, zip_filename)
-
-    # Clean up all temporary files
-    for file in os.listdir():
-        if file.endswith(".mp3") or file.endswith("_cut.mp3") or file == "output.mp3" or file == zip_filename:
-            os.remove(file)
-
-    st.success('Mashup file sent to email')
-
-def send_email(to, filename):
-    from_email = "your_email@gmail.com"  # Change to your own Gmail address
-    from_password = "your_password"  # Change to your own Gmail password
-    subject = "Mashup file"
-    message = "Please find attached the Mashup file"
-    file = filename
-
+def sendMail(recp):
+    # Email details
+    sender = st.secrets["SMTP_MAIL"]
+    recipient = recp
+    password = st.secrets["SMTP_PASS"]
+    subject = 'Combined Audio File'
+    # Create the message
     msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['To'] = to
+    msg['From'] = sender
+    msg['To'] = recipient
     msg['Subject'] = subject
+    # Add the text
+    text = MIMEText(f'Please find the attached the combined audio file of {singer}')
+    msg.attach(text)
+    # Add the audio file
+    audi = MIMEAudio(open('combined.mp3', 'rb').read(), 'mp3')
+    msg.attach(audi)
+    # Send the email
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(sender, password)
+    smtp.sendmail(sender, recipient, msg.as_string())
+    smtp.quit()
 
-    msg.attach(MIMEText(message))
+singer = st.text_input("Singer Name:")
+m = st.number_input("Nnumber of Videos:", min_value=10, max_value=50)
+n = st.number_input("Duration of each Audio Clip (in seconds):", min_value=20, max_value=90)
+recp = st.text_input("Email To:")
+bt1 = st.button("Combine and Send Mail")
 
-    with open(file, 'rb') as f:
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload(f.read())
-        encoders.encode_base64(part)
-        part.add_header
+
+if bt1:
+    # if download_audio_from_search(singer, n, m):
+    # combine_audio_files(singer, n, m)
+    # recp = st.text_input("Enter receiver email:")
+    with st.spinner('Preparing Audio... (this may take upto a few minutes)'):
+#         download_audio_from_search(singer, n, m)
+#         combine_audio_files(singer, n, m)
+#         sendMail(recp)
+#         st.success('Your file was mailed successfully!')
+        try:
+            download_audio_from_search(singer, n, m)
+            combine_audio_files(singer, n, m)
+            sendMail(recp)
+            st.success('Your file was mailed successfully!')
+        except:
+            st.error("Too much traffic at the moment, please try again in a few minutes")
+    
+        
+    # if st.button("Send Mail"):       
+    # st.success("Mail sent successfully!")
